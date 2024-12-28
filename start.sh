8b543cf7e3ee500317dd2c8883cce5328071020b
Aayushh Dutta <aayushhdutta2001@gmail.com>
2024-12-28 18:31:18 +0530

    Modified the start script for dynamic variable initiation (WORKDIR var)

--- start.sh
+++ start.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
-export WORK_DIR=$(pwd);
+export WORK_DIR=$(pwd)
 
-# Grafan needs permission to write data to the directory
+# Conditional statement for setting workdir variable when the program boots for the first time
+if grep -Fxq "WORKDIR=$WORK_DIR" ${WORK_DIR}/.env
+then
+    echo 
+else
+    sed -i "1iWORKDIR=$WORK_DIR" ${WORK_DIR}/.env
+fi
+
+# Grafana needs permission to write data to the directory
 sudo chmod -R 777 ${WORK_DIR}/grafana/storage
  
 sudo docker compose up -d
