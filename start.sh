0325c57f8b379884ff18b16d8182bdd1f3e23f14
Aayushh Dutta <aayushhdutta2001@gmail.com>
2024-12-29 17:53:55 +0530

    Modified the start script

--- start.sh
+++ start.sh
@@ -7,9 +7,9 @@ then
     echo 
 else
     sed -i "1iWORKDIR=$WORK_DIR" ${WORK_DIR}/.env
+    
+    # Grafana needs permission to write data to the directory
+    sudo chmod -R 777 ${WORK_DIR}/grafana/storage
 fi
-
-# Grafana needs permission to write data to the directory
-sudo chmod -R 777 ${WORK_DIR}/grafana/storage
  
 sudo docker compose up -d
