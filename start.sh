c9ceb7f77e880400f97f109ed2e63df6cf20936a
Aayushh Dutta <aayushhdutta2001@gmail.com>
2024-12-29 18:18:46 +0530

    Modified the start script to give folder permissions to docker

--- start.sh
+++ start.sh
@@ -7,9 +7,12 @@ then
     echo 
 else
     sed -i "1iWORKDIR=$WORK_DIR" ${WORK_DIR}/.env
-    
+
     # Grafana needs permission to write data to the directory
     sudo chmod -R 777 ${WORK_DIR}/grafana/storage
+    sudo chmod -R 777 ${WORK_DIR}/druid
+    sudo chmod -R 777 ${WORK_DIR}/zookeeper
+    sudo chmod -R 777 ${WORK_DIR}/postgres
 fi
  
 sudo docker compose up -d
